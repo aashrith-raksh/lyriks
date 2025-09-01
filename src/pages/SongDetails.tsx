fe343584bb94314456f0a60bcfb3e64dbf8b8345
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-09-02 01:38:16 +0530

    added header for song details page

--- src/pages/SongDetails.tsx
+++ src/pages/SongDetails.tsx
@@ -1,6 +1,28 @@
+import { useGetTrackDetailsByIdQuery } from "@/redux/services/shazamCore";
+import { useParams } from "react-router-dom";
+import Loader from "@/assets/loader.svg";
+import SongDetailsHeader from "@/components/song-details/SongDetailsHeader";
+
 
 function SongDetails() {
-  return <div>Song Details Page</div>;
+  const { songId } = useParams();
+
+  const { data, isFetching, isError } = useGetTrackDetailsByIdQuery({ songId });
+  if (isFetching) {
+    return <img src={Loader} width={130} />;
+  }
+
+  if (isError) {
+    return <p>Error</p>;
+  }
+
+
+  return (
+    <>
+    <SongDetailsHeader data={data}/>
+    </>
+    
+  );
 }
 
 export default SongDetails;
