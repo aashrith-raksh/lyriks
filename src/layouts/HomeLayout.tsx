7a41f195ed69fb42ba30240128e661cf959429bb
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-09-01 18:51:43 +0530

    added common layout for discover and song-details page

---
+++ src/layouts/HomeLayout.tsx
@@ -0,0 +1,17 @@
+import TopCharts from '@/components/discover/TopCharts'
+import { Outlet } from 'react-router-dom'
+
+const HomeLayout = () => {
+  return (
+    <section className="flex h-full max-h-screen gap-6 p-6">
+      <div className="flex h-full flex-col flex-1 gap-6">
+        <Outlet/>
+      </div>
+      <div className="basis-1/3 max-md:hidden max-h-full">
+        <TopCharts />
+      </div>
+    </section>
+  )
+}
+
+export default HomeLayout
--- src/pages/Discover.tsx
+++ src/pages/Discover.tsx
@@ -2,24 +2,18 @@ import DataDisplay from "@/components/discover/DataDisplay";
 import DiscoverPageHeader from "@/components/discover/DiscoverHeader";
 import MusicPlayer from "@/components/discover/MusicPlayer";
 import SongCard, { type SongCardArgs } from "@/components/discover/SongCard";
-import TopCharts from "@/components/discover/TopCharts";
 
 function Discover() {
   return (
-    <section className="flex h-full max-h-screen gap-6 p-6">
-      <div className="flex h-full flex-col flex-1 gap-6">
-        <DiscoverPageHeader />
-        <DataDisplay displayCardVariant="songCard">
-          {(song: Partial<SongCardArgs>, idx: number) => (
-            <SongCard {...song} songIndex={idx} />
-          )}
-        </DataDisplay>
-        <MusicPlayer />
-      </div>
-      <div className="basis-1/3 max-md:hidden max-h-full">
-        <TopCharts />
-      </div>
-    </section>
+    <>
+      <DiscoverPageHeader />
+      <DataDisplay displayCardVariant="songCard">
+        {(song: Partial<SongCardArgs>, idx: number) => (
+          <SongCard {...song} songIndex={idx} />
+        )}
+      </DataDisplay>
+      <MusicPlayer />
+    </>
   );
 }
 
