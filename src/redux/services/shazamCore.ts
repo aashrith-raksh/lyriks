a9c352ee0666525c8a015559bf4230ac7771313a
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-09-19 01:18:16 +0530

    added a query to fetch top charts around a location

--- src/redux/services/shazamCore.ts
+++ src/redux/services/shazamCore.ts
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { TopChartsResponse } from "./types/get-top-charts-response";
 import type { TrackDetailsResponse } from "./types/get-track-details-response";
 import type { ArtistDetailsResponse } from "./types/get-artist-details-response";
+import type { ChartsByCountryResponse } from "./types/get-charts-by-country-response";
 
 type GetTopChartsArgs = {
   genre_code?: string;
@@ -9,21 +10,21 @@ type GetTopChartsArgs = {
 };
 
 type GetTrackDetailsArgs = {
-  songId?: number | string
-}
+  songId?: number | string;
+};
 
 type GetArtistDetailsArgs = {
-  artistId?: number | string
-}
+  artistId?: number | string;
+};
+type GetChartsByCountryCodeArgs = {
+  countryCode?: null | string;
+};
 const shazamCoreApi = createApi({
   reducerPath: "shazamCoreApi",
   baseQuery: fetchBaseQuery({
     baseUrl: `https://${import.meta.env.VITE_RAPIDAPI_API_HOST}`,
     prepareHeaders: (headers) => {
-      headers.set(
-        "x-rapidapi-key",
-        import.meta.env.VITE_RAPIDAPI_API_KEY
-      );
+      headers.set("x-rapidapi-key", import.meta.env.VITE_RAPIDAPI_API_KEY);
       headers.set("x-rapidapi-host", "shazam-core.p.rapidapi.com");
       return headers;
     },
@@ -33,14 +34,32 @@ const shazamCoreApi = createApi({
       query: ({ genre_code = "POP", country_code = "DZ" }) =>
         `/v1/charts/genre-world?genre_code=${genre_code}&country_code=${country_code}`,
     }),
-    getTrackDetailsById: builder.query<TrackDetailsResponse, GetTrackDetailsArgs>({
-      query: ({songId}) => `/v2/tracks/details?track_id=${songId}`
+    getTrackDetailsById: builder.query<
+      TrackDetailsResponse,
+      GetTrackDetailsArgs
+    >({
+      query: ({ songId }) => `/v2/tracks/details?track_id=${songId}`,
     }),
-    getArtistDetailsById: builder.query<ArtistDetailsResponse, GetArtistDetailsArgs>({
-      query: ({artistId}) => `v2/artists/details?artist_id=${artistId}`
-    })
+    getArtistDetailsById: builder.query<
+      ArtistDetailsResponse,
+      GetArtistDetailsArgs
+    >({
+      query: ({ artistId }) => `v2/artists/details?artist_id=${artistId}`,
+    }),
+    getChartsByCountryCode: builder.query<
+      ChartsByCountryResponse[],
+      GetChartsByCountryCodeArgs
+    >({
+      query: ({ countryCode }) =>
+        `v1/charts/country?country_code=${countryCode}`,
+    }),
   }),
 });
 
-export const { useGetTopCharsQuery, useGetTrackDetailsByIdQuery, useGetArtistDetailsByIdQuery } = shazamCoreApi;
+export const {
+  useGetTopCharsQuery,
+  useGetTrackDetailsByIdQuery,
+  useGetArtistDetailsByIdQuery,
+  useGetChartsByCountryCodeQuery,
+} = shazamCoreApi;
 export default shazamCoreApi;
---
+++ src/redux/services/types/get-charts-by-country-response.ts
@@ -0,0 +1,126 @@
+export interface ChartsByCountryResponse {
+ attributes:    Attributes;
+ href:          string;
+ id:            string;
+ meta:          Meta;
+ relationships: Relationships;
+ type:          RootObjectType;
+}
+
+export interface Attributes {
+ albumName:                 string;
+ artistName:                string;
+ artwork:                   Artwork;
+ audioLocale:               AudioLocale;
+ audioTraits:               AudioTrait[];
+ composerName?:             string;
+ contentRating?:            ContentRating;
+ discNumber:                number;
+ durationInMillis?:         number;
+ genreNames:                string[];
+ hasLyrics:                 boolean;
+ hasTimeSyncedLyrics:       boolean;
+ isAppleDigitalMaster:      boolean;
+ isMasteredForItunes:       boolean;
+ isVocalAttenuationAllowed: boolean;
+ isrc:                      string;
+ name:                      string;
+ playParams?:               PlayParams;
+ previews:                  Preview[];
+ releaseDate:               Date;
+ trackNumber:               number;
+ url:                       string;
+}
+
+export interface Artwork {
+ bgColor:    string;
+ hasP3:      boolean;
+ height:     number;
+ textColor1: string;
+ textColor2: string;
+ textColor3: string;
+ textColor4: string;
+ url:        string;
+ width:      number;
+}
+
+export enum AudioLocale {
+ Ar = "ar",
+ EnUS = "en-US",
+ EsES = "es-ES",
+ Fi = "fi",
+ FrFR = "fr-FR",
+ Ig = "ig",
+ Nl = "nl",
+ PtPT = "pt-PT",
+ Ru = "ru",
+ Sq = "sq",
+ ZhHansCN = "zh-Hans-CN",
+ Zxx = "zxx",
+}
+
+export enum AudioTrait {
+ Atmos = "atmos",
+ HiResLossless = "hi-res-lossless",
+ Lossless = "lossless",
+ LossyStereo = "lossy-stereo",
+ Spatial = "spatial",
+}
+
+export enum ContentRating {
+ Explicit = "explicit",
+}
+
+export interface PlayParams {
+ id:   string;
+ kind: Kind;
+}
+
+export enum Kind {
+ Song = "song",
+}
+
+export interface Preview {
+ url: string;
+}
+
+export interface Meta {
+ contentVersion: ContentVersion;
+ formerIds?:     string[];
+}
+
+export interface ContentVersion {
+ MZ_INDEXER: number;
+ RTCI:       number;
+}
+
+export interface Relationships {
+ artists:        Artists;
+ "music-videos": MusicVideos;
+}
+
+export interface Artists {
+ data: Datum[];
+ href: string;
+}
+
+export interface Datum {
+ href: string;
+ id:   string;
+ type: DatumType;
+}
+
+export enum DatumType {
+ Artists = "artists",
+ MusicVideos = "music-videos",
+}
+
+export interface MusicVideos {
+ data:  Datum[];
+ href:  string;
+ next?: string;
+}
+
+export enum RootObjectType {
+ Songs = "songs",
+}
