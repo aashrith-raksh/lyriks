b6368cceb6db7f9739d0ddbc07075d628bec46aa
Aashrith Raksh <aashrith.raksh@gmail.com>
2025-09-21 20:22:11 +0530

    added a TopArtists page

--- src/pages/TopArtists.tsx
+++ src/pages/TopArtists.tsx
@@ -1,6 +1,67 @@
+import Error, { type CustomErrorResponseType } from "@/components/Error";
+import Loading from "@/components/Loading";
+import type { SongCardArgs } from "@/components/SongCard";
+import SongCard from "@/components/SongCard";
+import { cn } from "@/lib/utils";
+import { useGetTopCharsQuery } from "@/redux/services/shazamCore";
+import type { FetchBaseQueryError } from "@reduxjs/toolkit/query";
+import { useMemo, type CSSProperties } from "react";
 
 function TopArtists() {
-  return <div>Top Artists Page</div>;
+  const { data, error, isFetching, isError } = useGetTopCharsQuery({});
+  let content = <Loading />;
+
+  const filteredData: SongCardArgs[] | undefined = useMemo(() => {
+    if (data) {
+      return data.slice(0, 20).map<SongCardArgs>((item, idx) => ({
+        id: item.id,
+        attributes: {
+          albumName: item.attributes.albumName,
+          artwork: {
+            url: item.attributes.artwork.url,
+          },
+          artistName: item.attributes.artistName,
+        },
+        relationships: item.relationships,
+        songIndex: idx,
+        songCategory: "around-you",
+      }));
+    }
+  }, [data]);
+
+  if (!isFetching) {
+    if (isError) {
+      const errorData = (error as FetchBaseQueryError)?.data;
+      const errorMessage =
+        (errorData as CustomErrorResponseType)?.detail ||
+        "Please try again after some time";
+
+      content = <Error errorMessage={errorMessage} />;
+    } else {
+      content = (
+        <section>
+          <h1 className="text-4xl mb-4 font-bold">Top Artists</h1>
+
+          <ul
+            className={cn("grid auto-grid gap-4")}
+            style={
+              {
+                "--min-col-size": "200px",
+              } as CSSProperties
+            }
+          >
+            {filteredData!.map((item) => (
+              <li>
+                <SongCard {...item} clickable={false}/>
+              </li>
+            ))}
+          </ul>
+        </section>
+      );
+    }
+  }
+
+  return content;
 }
 
 export default TopArtists;
